02a7cfebb76c6aaeae701240fbd00ca2fd7c2148
Saif-Rahman-801 <saifurs102@gmail.com>
2023-08-28 16:38:47 +0600

    handle search added

--- js/index.js
+++ js/index.js
@@ -1,29 +1,30 @@
 console.log("Hello World");
-const loadPhone = async () => {
+const loadPhone = async (searchField) => {
   const res = await fetch(
-    "https://openapi.programming-hero.com/api/phones?search=iphone"
+    `https://openapi.programming-hero.com/api/phones?search=${searchField}`
   );
   const data = await res.json();
   //   console.log(data.data);
   const phones = data.data;
   displayPhones(phones);
 };
-loadPhone();
+
 const displayPhones = (phones) => {
   // step1 -> get the div
   const phoneContainer = document.getElementById("phone-container");
+  phoneContainer.textContent = "";
   phones.forEach((phone) => {
-    console.log(phone);
+    // console.log(phone);
     // step 2 - Create a div
     const div = document.createElement("div");
-    div.classList = `card w-96 bg-base-100 shadow-xl m-4`;
+    div.classList = `card p-4 rounded-sm bg-base-100 shadow-xl m-4`;
     div.innerHTML = `
     <figure><img src=${phone.image} alt="Shoes" /></figure>
-    <div className="card-body">
-      <h2 className="card-title">${phone.phone_name}!</h2>
+    <div class="card-body">
+      <h2 class="card-title">${phone.phone_name}!</h2>
       <p>If a dog chews shoes whose shoes does he choose?</p>
-      <div className="card-actions justify-end">
-        <button className="btn btn-primary">Buy Now</button>
+      <div class="card-actions justify-end">
+        <button class="btn btn-primary">Buy Now</button>
       </div>
     </div>
     `;
@@ -31,3 +32,12 @@ const displayPhones = (phones) => {
     phoneContainer.appendChild(div);
   });
 };
+
+// Handle search
+const handleSearch = () => {
+  console.log("Search");
+  const searchField = document.getElementById("search-field").value;
+  loadPhone(searchField);
+};
+
+// loadPhone();
